450cd06e8776f3c7a78d2edc52894aa0d3d60ddc
joshknopp <github@joshknopp.com>
2021-11-16 02:33:17 +0000

    Scaffolding

---
+++ src/main/java/com/joshknopp/tddset/TddSetApplication.java
@@ -0,0 +1,13 @@
+package com.joshknopp.tddset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshknopp/tddset/TddSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshknopp.tddset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
