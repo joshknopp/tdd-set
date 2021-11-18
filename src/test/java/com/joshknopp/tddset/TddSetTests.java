505a90d9e5c7c95e977f4c160b4be21500395b76
joshknopp <github@joshknopp.com>
2021-11-18 04:06:03 +0000

    A failing test (red)

--- src/test/java/com/joshknopp/tddset/TddSetTests.java
+++ src/test/java/com/joshknopp/tddset/TddSetTests.java
@@ -5,14 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TddSetTests {
-    private TddSet tddSet = new TddSet<>();
+    @InjectMocks
+    private TddSet tddSet;
 
     @Test
     void itShouldImplementSetInterface() {
         assertTrue(tddSet instanceof Set);
+    }
+
+    @Test
+    void isEmptyShouldReturnTrueIfEmpty() {
+        assertTrue(tddSet.isEmpty());
     }
 }
