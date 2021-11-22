9cf2705048a741f5a5e027460c1bacadb9f50852
joshknopp <github@joshknopp.com>
2021-11-21 23:14:29 -0500

    Add a couple of failing tests; expose a hole in add() and start thinking about remove()

--- src/test/java/com/joshknopp/tddset/TddSetTests.java
+++ src/test/java/com/joshknopp/tddset/TddSetTests.java
@@ -1,14 +1,13 @@
 package com.joshknopp.tddset;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Set;
-
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TddSetTests {
@@ -20,13 +19,38 @@ public class TddSetTests {
         assertTrue(tddSet instanceof Set);
     }
 
+    @Test
+    void sizeShouldReturnZeroForNewSet() {
+        assertEquals(0, tddSet.size());
+    }
+
+    @Test
+    void sizeShouldReturnOneAfterAdd() {
+        tddSet.add("foo");
+        assertEquals(1, tddSet.size());
+    }
+
+    @Test
+    void sizeShouldReturnOneAfterDuplicateAdd() {
+        tddSet.add("foo");
+        tddSet.add("foo");
+        assertEquals(1, tddSet.size());
+    }
+
+    @Test
+    void sizeShouldReturnZeroAfterAddRemove() {
+        tddSet.add("foo");
+        tddSet.add("remove");
+        assertEquals(1, tddSet.size());
+    }
+
     @Nested
     class isEmptyShould {
         @Test
         void returnTrueIfEmpty() {
             assertTrue(tddSet.isEmpty());
         }
-    
+
         @Test
         void returnFalseIfNotEmpty() {
             tddSet.add("foo");
